ac5cd79287a2afa25bc987b254895918b88942c2
Sasha Osin <sashaosin364@gmail.com>
2023-02-02 22:33:24 +0700

    add dynamic url

--- coolsite/women/views.py
+++ coolsite/women/views.py
@@ -3,17 +3,32 @@ from django.http import HttpResponse,HttpResponseNotFound
 
 from .models import *
 
-menu =['О сайте','Добавить статью','Обратная связь','Войти']
+menu =[{'title':'О сайте','url_name':'about'},
+       {'title':'Добавить статью','url_name':'add_page'},
+       {'title':'Обратная связь','url_name':'contact'},
+       {'title':'Войти','url_name':'login'}]
 
 def index(request):
     posts = Women.objects.all()
-    return render(request,'women/index.html',{'posts':posts,'menu':menu,'title':'Главная страница'})
+    context = {'posts':posts,
+               'menu':menu,
+               'title':'Главная страница'}
+    return render(request,'women/index.html',context=context)
 
 def about(request):
     return render(request, 'women/about.html',{'menu':menu,'title':'О странице'})
 
-def categories(request,catid):
-    return HttpResponse(f'<h1>Категории</h1><p>{catid}</p>')
+def addpage(request):
+    return HttpResponse('Добавить статью')
+
+def contact(request):
+    return HttpResponse('Обратная связь')
+
+def login(request):
+    return HttpResponse('Авторизация')
 
 def pageNotFound(request,exception):
     return HttpResponseNotFound('<h1>Not found</h1>')
+
+def show_post(request,post_id):
+    return HttpResponse(f'Статья номер {post_id}')
